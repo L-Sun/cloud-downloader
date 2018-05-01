396592113acd0d2cc436ef0853de726c76914a73
L <zover.v@gmail.com>
2018-05-01 19:28:33 +0800

    update state saga

--- src/sagas/format.js
+++ src/sagas/format.js
@@ -9,10 +9,28 @@ const formatSize = (size, type="size") => {
   const index = Math.floor((size.length - 1) / 3)
   const value = (parseInt(size, 10) / 1000 ** index).toFixed(2)
   if (type==="size") {
-    return value.toString() + ['B', 'KB', 'MB', 'GB'][index]
+    return value + ['B', 'KB', 'MB', 'GB'][index]
   } else if(type==="speed") {
-    return value.toString() + ['B/s', 'KB/s', 'MB/s', 'GB/s'][index]
+    return value + ['B/s', 'KB/s', 'MB/s', 'GB/s'][index]
   }
+}
+
+const formatTime = (completedLength, totalLength, speed) => {
+  let hr = 0, min = 0, sec = 0
+  const remaningLength = parseInt(totalLength, 10) - parseInt(completedLength, 10)
+  sec = parseInt(remaningLength / parseInt(speed, 10), 10)
+  if (sec > 60) {
+    min = parseInt(sec / 60, 10)
+    sec = parseInt(sec % 60, 10)
+    if (min > 60) {
+      hr = parseInt(min / 60, 10)
+      min = parseInt(min % 60, 10)
+    }
+  }
+  hr < 10 && (hr = '0' + hr)
+  min < 10 && (min = '0' + min)
+  sec < 10 && (sec = '0' + sec)
+  return hr + ':' + min + ':' + sec
 }
 
 const formatTask = (task) => {
@@ -24,6 +42,7 @@ const formatTask = (task) => {
     progress: formatProgress(task.completedLength, task.totalLength),
     uploadSpeed: formatSize(task.uploadSpeed, "speed"),
     downloadSpeed: formatSize(task.downloadSpeed, "speed"),
+    remainingTime: formatTime(task.completedLength, task.totalLength, task.downloadSpeed),
     files: formatFiles(task.files)
   }
 
