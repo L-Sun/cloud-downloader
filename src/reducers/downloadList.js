39fe911ffe07e1c92879aa2f3be60f727314da5f
L <zover.v@gmail.com>
2018-09-18 07:48:20 +0800

    fixed downloadList: update incorectely after removing

--- src/reducers/downloadList.js
+++ src/reducers/downloadList.js
@@ -3,7 +3,7 @@ const initialState = {}
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_LIST_SUCCESS':
-      return { ...state, ...action.payload }
+      return { ...action.payload }
     default:
       return state
   }
